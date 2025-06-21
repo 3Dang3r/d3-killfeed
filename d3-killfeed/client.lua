7d2fd92b560320f1f0bae2ed82d33acef9f5b280
Dang3r <babygang21@proton.me>
2025-06-22 01:54:24 +0300

    Update client.lua

--- d3-killfeed/client.lua
+++ d3-killfeed/client.lua
@@ -1,30 +1,22 @@
 local Config = {
-    Position = "center-right" -- options: top-left, top-right, bottom-left, bottom-right, center-left, center-right
+    Position = "center-right"
 }
 
 Citizen.CreateThread(function()
-    Citizen.Wait(1000)
-
-    SendNUIMessage({
-        action = "setPosition",
-        position = Config.Position
-    })
-
-    SendNUIMessage({
-        action = "toggle",
-        show = true
-    })
+    Wait(1000)
+    SendNUIMessage({ action = "setPosition", position = Config.Position })
+    SendNUIMessage({ action = "toggle", show = true })
 end)
+
 
 RegisterCommand("testkillfeed", function()
     TriggerServerEvent("killfeed:reportKill", "Dang3r", "Player")
     TriggerServerEvent("killfeed:reportKill", "Player", "Dang3r")
-    Citizen.Wait(2000)
-    TriggerServerEvent("killfeed:reportKill", "Dang3r", "Dang3r") -- Suicide Test
+    Wait(2000)
+    TriggerServerEvent("killfeed:reportKill", "Dang3r", "Dang3r")
 end, false)
 
-RegisterNetEvent("killfeed:showKill")
-AddEventHandler("killfeed:showKill", function(killer, victim)
+RegisterNetEvent("killfeed:showKill", function(killer, victim)
     local isSuicide = killer == victim
     SendNUIMessage({
         action = "addKill",
@@ -36,28 +28,16 @@ AddEventHandler("killfeed:showKill", function(killer, victim)
     })
 end)
 
-AddEventHandler('gameEventTriggered', function(name, args)
-    if name == 'CEventNetworkEntityDamage' then
-        local victim = args[1]
-        local attacker = args[2]
 
-        if not IsEntityAPed(victim) or not IsEntityAPed(attacker) then return end
-        if not IsPedAPlayer(victim) then return end
+AddEventHandler('playerDied', function(killerId, weaponHash)
+    local victimName = GetPlayerName(PlayerId())
+    local killerName = "Unknown"
 
-        local victimId = NetworkGetPlayerIndexFromPed(victim)
-        local attackerId = NetworkGetPlayerIndexFromPed(attacker)
-
-        if victimId == -1 then return end
-
-        local victimName = (GetPlayerFromServerId(GetPlayerServerId(victimId)) ~= -1) and GetPlayerName(victimId) or "Unknown"
-
-        if attackerId == -1 or victimId == attackerId then
-            -- Suicide
-            TriggerServerEvent("killfeed:reportKill", victimName, victimName)
-        else
-            -- Player kill
-            local killerName = (GetPlayerFromServerId(GetPlayerServerId(attackerId)) ~= -1) and GetPlayerName(attackerId) or "Unknown"
-            TriggerServerEvent("killfeed:reportKill", killerName, victimName)
-        end
+    if killerId ~= nil and killerId ~= -1 and killerId ~= PlayerId() then
+        killerName = GetPlayerName(killerId)
+        TriggerServerEvent("killfeed:reportKill", killerName, victimName)
+    else
+        -- Suicide
+        TriggerServerEvent("killfeed:reportKill", victimName, victimName)
     end
 end)
